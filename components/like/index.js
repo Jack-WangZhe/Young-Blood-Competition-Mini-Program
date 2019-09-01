0f75fbb469983faa5b7d72ed86a322a99d1b5c72
Jack wangzhe <674112795@qq.com>
2019-09-01 20:43:55 +0800

    组件数据/事件/属性

--- components/like/index.js
+++ components/like/index.js
@@ -1,25 +1,44 @@
-// components/like/index.js
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    likeStatus: {
+      // type是必填字段,表示属性的类型
+      type: Boolean,
+      // value是选填字段,表示属性的默认值
+      value: false,
+      // observer是选填字段
+      observer: ()=> {}
+    },
+    count: {
+      type: Number
+      // Number类型value默认是0
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    /* 定义变量count,数据绑定 */
+    likePicUrl: 'images/like.png',
+    disLikePicUrl: 'images/like@dis.png'
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-    onLike: (event) => {
-      console.log(event)
+    onLike: function(event) {
+      let like = this.properties.likeStatus;
+      let count = this.properties.count;
+      
+      // this.setData({})指明要更新的变量
+      this.setData({
+        count: like?count-1:count+1,
+        likeStatus: !like
+      });
     }
   }
 })
